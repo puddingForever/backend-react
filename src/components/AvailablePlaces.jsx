4ec46ca3746af66a157b2bb80c68296e1f074169
BytePudding <developerjieun94@gmail.com>
2025-04-23 21:57:55 +0900

    initial setting

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import Places from './Places.jsx';
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces ]= useState([]);
+
+  useEffect(() => {
+
+    const fetchPlaces = async () => {
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places)
+    }
+
+    fetchPlaces();
+  },[])
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
